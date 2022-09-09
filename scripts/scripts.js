44313e8175ef5f84aae4302e5b1ed9e1f5e74378
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-09 22:12:03 +0300

    added some html divs and bgs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,14 @@
+const dog = document.getElementById("test")
 let gender = fetch("https://api.genderize.io/?name=sarah"
 ).then(response =>{
     return response.json()
 }).then(text => console.log(text.gender))
+
+
+fetch("https://dog.ceo/api/breeds/image/random"
+).then(response =>{
+    return response.json()
+}).then(text => {
+    console.log(text.message)
+    dog.innerHTML = `<img src="${text.message}"/>`
+})
